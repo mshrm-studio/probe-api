696660148dcafa9917a8b612c9abca9ac5ef8b76
thomcm <tmaltby19@gmail.com>
2023-11-03 14:25:03 +0700

    better leverage inheritance

--- app/Services/LilNounsService.php
+++ app/Services/LilNounsService.php
@@ -2,19 +2,14 @@
 
 namespace App\Services;
 
-use App\Contracts\ERC721ServiceContract;
-use App\Contracts\NounsServiceContract;
+use App\Services\BaseNounsService;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Cache;
 use Web3\Web3;
 use Web3\Contract;
-use Web3\Utils;
 
-class LilNounsService implements ERC721ServiceContract, NounsServiceContract {
-    protected $web3;
-    protected $contract;
-
+class LilNounsService extends BaseNounsService {
     public function __construct(Web3 $web3) {
         $this->web3 = $web3;
 
@@ -27,129 +22,7 @@ class LilNounsService implements ERC721ServiceContract, NounsServiceContract {
         });
 
         $this->contract = new Contract($web3->provider, json_encode($abi));
-        $contractAddress = config('services.lil_nouns_contract.address');
-        $this->contract->at($contractAddress);
-    }
-
-    public function getMintEvent(int $tokenId): array 
-    {
-        $mintEvent = [];
-
-        $filters = [
-            'fromBlock' => '0x0',  // Start searching from the first block
-            'toBlock' => 'latest', // Search until the latest block
-            'address' => config('services.lil_nouns_contract.address'), // The contract address
-            'topics' => [
-                Utils::sha3('Transfer(address,address,uint256)'), // This is the topic for the Transfer event
-                null, // The 'from' address, which is null for mint events (zero address)
-                null, // The 'to' address, which we're not filtering by
-                '0x' . str_pad(dechex($tokenId), 64, '0', STR_PAD_LEFT), // The token ID, padded to 32 bytes
-            ],
-        ];
-
-        $this->web3->eth->getLogs($filters, function ($err, $logs) use (&$mintEvent) {
-            if ($err) {
-                throw new \Exception($err->getMessage());
-            }
-
-            if (count($logs) > 0) {
-                $mintEvent = is_object($logs[0]) ? (array) $logs[0] : $logs[0];
-            }
-        });
-
-        if (empty($mintEvent)) {
-            throw new \Exception("No mint event found for token ID {$tokenId}");
-        }
-
-        return $mintEvent;
-    }
-
-    public function getBlock(string $blockNumber): array 
-    {
-        $blockDetails = [];
-
-        $this->web3->eth->getBlockByNumber($blockNumber, true, function ($err, $block) use (&$blockDetails) {
-            if ($err !== null) {
-                throw new \Exception($err->getMessage());
-            }
-
-            $blockDetails = is_object($block) ? (array) $block : $block;
-        });
-
-        return $blockDetails;
-    }
-
-    public function getTotalSupply(): int
-    {
-        $totalSupply = 0;
-
-        $this->contract->call('totalSupply', [], function ($err, $result) use (&$totalSupply) {
-            if ($err) {
-                throw new \Exception($err->getMessage());
-            }
-
-            $resultBigInteger = $result[0] instanceof \phpseclib\Math\BigInteger ? $result[0] : null;
-            
-            if ($resultBigInteger) {
-                $totalSupply = (int) $resultBigInteger->toString();
-            }
-        });
-
-        return $totalSupply;
-    }
-
-    public function getTokenByIndex(int $index): ?int
-    {
-        $tokenId = null;
-
-        $this->contract->call('tokenByIndex', $index, function ($err, $result) use (&$tokenId) {
-            if ($err) {
-                throw new \Exception($err->getMessage());
-            }
-
-            $resultBigInteger = $result[0] instanceof \phpseclib\Math\BigInteger ? $result[0] : null;
-
-            if ($resultBigInteger) {
-                $tokenId = (int) $resultBigInteger->toString();
-            }
-        });
-
-        return $tokenId;
-    }
-
-    public function getTokenURI(int $tokenId): ?string
-    {
-        $tokenURI = null;
-
-        $this->contract->call('tokenURI', $tokenId, function ($err, $result) use (&$tokenURI) {
-            if ($err) {
-                throw new \Exception($err->getMessage());
-            }
-
-            if (gettype($result[0] ?? null) == 'string') {
-                $tokenURI = $result[0];
-            }
-        });
-
-        return $tokenURI;
-    }
-
-    public function getSeeds(int $tokenId): array
-    {
-        $seeds = [];
-
-        $this->contract->call('seeds', $tokenId, function ($err, $result) use (&$seeds) {
-            if ($err) {
-                throw new \Exception($err->getMessage());
-            }
-
-            foreach ($result as $key => $value) {
-                if ($value instanceof \phpseclib\Math\BigInteger) {
-                    $seeds[$key] = (int) $value->toString();
-                }
-            }
-        });
-
-        return $seeds;
+        $this->contractAddress = config('services.lil_nouns_contract.address');
+        $this->contract->at($this->contractAddress);
     }
 }
