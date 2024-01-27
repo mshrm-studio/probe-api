78b8ec46ccefabbf646d07526f7ef91687ecb59e
Tom <tmaltby19@gmail.com>
2024-01-27 16:47:10 +0700

    color syncing work contd.

--- app/Jobs/Noun/SyncNounTokenColors.php
+++ app/Jobs/Noun/SyncNounTokenColors.php
@@ -33,39 +33,161 @@ class SyncNounTokenColors implements ShouldQueue
             ->limit(50)
             ->get();
 
-        // TODO: check for emptiness of color column when migrated
-
         foreach ($nouns as $noun) {
-            
-            // Read the SVG file content
-            $svgContent = Storage::get($noun->svg_path);
-            $xml = simplexml_load_string($svgContent);
-            $namespaces = $xml->getNamespaces(true);
-        
-            $colors = [];
+            $imagick = new \Imagick();
 
-            $width = (string) $xml['width'];
-            $height = (string) $xml['height'];
-                    
-            // Specify the namespace if needed
-            $svgNamespace = $namespaces[''] ?? null;
+            $svgContent = Storage::get($noun->svg_path);
+            
+            $imagick->readImageBlob($svgContent);
+            
+            $imagick->setImageFormat('png24');
+
+            // Get background colour to ignore
+            $backgroundColor = $this->getBackgroundColor($svgContent);
+                                
+            // Process the noun to get histogram/area
+            $histogram = $imagick->getImageHistogram();
+
+            // Get area
+            $area = $this->calculateArea($histogram, $backgroundColor);
+            
+            // Get the weight
+            $weight = $this->calculateWeight($histogram, $backgroundColor);
+
+            // Build response object and return
+            $nounStats = [];
+            $nounStats['histogram'] = $this->formatHistogram($histogram);
+            $nounStats['area'] = $area;
+            $nounStats['weight'] = $weight;
+
+            // Release memory
+            $imagick->clear();
+            $imagick->destroy();
+            
+            \Log::info('***********');
+            \Log::info('Noun ' . $noun->id);
+            \Log::info('area');
+            \Log::info($nounStats['area']);
+            \Log::info('weight');
+            \Log::info($nounStats['weight']);
+            \Log::info('histogram');
+            \Log::info($nounStats['histogram']);
+        }   
+    }
+
+    private function calculateArea(array $histogram, string $backgroundColor): int
+    {
+        $area = 0;
+
+        $backgroundColorRGB = $this->convertHexToRGB($backgroundColor);
         
-            // Loop through rect elements
-            foreach ($xml->children($svgNamespace)->rect as $rect) {
-                $attributes = $rect->attributes();
-        
-                if (isset($attributes->fill)) {
-                    $color = (string) $attributes->fill;
-                    $colors[] = $color; // Collect colors
-                }
+        // Iterate over histogram and add all pixels that aren't background color
+        foreach ($histogram as $pixel) {
+            $colorRGB = $pixel->getColor();
+            
+            if (
+                $colorRGB['r'] != $backgroundColorRGB['r'] && 
+                $colorRGB['g'] != $backgroundColorRGB['g'] && 
+                $colorRGB['b'] != $backgroundColorRGB['b']
+            ) {
+                $area += $pixel->getColorCount();
             }
+        }
         
-            \Log::info('**************************');
-            \Log::info("Token ID: $noun->token_id");
-            \Log::info("Width: $width, Height: $height");
-            $colorCount = count($colors);
-            \Log::info("Color count: $colorCount");
-            \Log::info($colors);
-        }        
+        return $area;
+    }
+
+    private function calculateWeight(array $histogram, string $backgroundColor): int
+    {
+        $weight = 0;
+
+        $backgroundColorRGB = $this->convertHexToRGB($backgroundColor);
+
+        // Iterate over histogram and add all pixels that aren't background color
+        foreach ($histogram as $pixel) {
+            $colorRGB = $pixel->getColor();
+
+            if (
+                $colorRGB['r'] != $backgroundColorRGB['r'] && 
+                $colorRGB['g'] != $backgroundColorRGB['g'] && 
+                $colorRGB['b'] != $backgroundColorRGB['b']
+            ) {
+                // Get avg pixel (grey scale) 
+                $greyScalePixel = ($colorRGB['r'] + $colorRGB['g'] + $colorRGB['b']) / 3;
+
+                // Times the pixel count for the color by the grey scale pixel (mass for color)
+                $weight += ($pixel->getColorCount() * $greyScalePixel);
+            }
+        }
+    
+        return $weight;
+    }
+
+    private function getBackgroundColor(string $svg): string 
+    {
+        // LoadXML
+        $xml = simplexml_load_string($svg);
+
+        // Get 100% fill color
+        $backgroundElement = $xml->rect[0];
+
+        return $backgroundElement["fill"];
+    }
+
+    private function convertHexToRGB(string $hexColor): array
+    {
+        sscanf($hexColor, "rgb(%d,%d,%d)", $r, $g, $b);
+
+        return ['r' => $r, 'g' => $g, 'b' => $b];
+    }
+    
+    private function formatHistogram(array $histogram): array
+    {
+        $formattedHistogram = [];
+        
+        foreach($histogram as $pixel) {
+            $colors = $pixel->getColor();
+
+            $key = sprintf("#%02x%02x%02x", $colors['r'], $colors['g'], $colors['b']);
+                   
+            $formattedHistogram[$key] = $pixel->getColorCount();
+            $formattedHistogram['rgb(' . $colors['r'] . ',' . $colors['g'] . ',' . $colors['b'] . ')'] = $pixel->getColorCount();
+        }
+    
+        return $formattedHistogram;
     }
 }
+
+
+        // foreach ($nouns as $noun) {
+            
+        //     // Read the SVG file content
+        //     $svgContent = Storage::get($noun->svg_path);
+        //     $xml = simplexml_load_string($svgContent);
+        //     $namespaces = $xml->getNamespaces(true);
+        
+        //     $colors = [];
+
+        //     $width = (string) $xml['width'];
+        //     $height = (string) $xml['height'];
+                    
+        //     // Specify the namespace if needed
+        //     $svgNamespace = $namespaces[''] ?? null;
+        
+        //     // Loop through rect elements
+        //     foreach ($xml->children($svgNamespace)->rect as $rect) {
+        //         $attributes = $rect->attributes();
+        
+        //         if (isset($attributes->fill)) {
+        //             $color = (string) $attributes->fill;
+        //             $colors[] = $color; // Collect colors
+        //         }
+        //     }
+        
+        //     \Log::info('**************************');
+        //     \Log::info("Token ID: $noun->token_id");
+        //     \Log::info("Width: $width, Height: $height");
+        //     $colorCount = count($colors);
+        //     \Log::info("Color count: $colorCount");
+        //     \Log::info($colors);
+        // }     
