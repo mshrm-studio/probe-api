3d6c60752f71714cd1d1e0fada57912d19e600c8
Tom <tmaltby19@gmail.com>
2024-03-09 18:31:49 +0700

    update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -46,7 +46,26 @@ class Kernel extends ConsoleKernel
             $schedule->job(new SyncNounTokenMintTimes, 'nouns')->everyMinute();
             $schedule->job(new SyncNounTokenColors, 'nouns')->everyMinute();
         }
-        else
+        else if (config('app.env') == 'staging') 
+        {
+            $schedule->job(new SyncLilNounTotalSupply, 'lils')->everyTenMinutes();
+            $schedule->job(new SyncLilNounTokenIdentities, 'lils')->everyTenMinutes();
+            $schedule->job(new SyncLilNounTokenImages, 'lils')->everyTenMinutes();
+            $schedule->job(new SyncLilNounTokenSeeds, 'lils')->everyTenMinutes();
+            $schedule->job(new SyncLilNounTokenSeedNames, 'lils')->everyTenMinutes();
+            $schedule->job(new SyncLilNounTokenBlockNumbers, 'lils')->everyTenMinutes();
+            $schedule->job(new SyncLilNounTokenMintTimes, 'lils')->everyTenMinutes();
+
+            $schedule->job(new SyncNounTotalSupply, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenIdentities, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenImages, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenSeeds, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenSeedNames, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenBlockNumbers, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenMintTimes, 'nouns')->everyMinute();
+            $schedule->job(new SyncNounTokenColors, 'nouns')->everyMinute();
+        }
+        else if (config('app.env') == 'production') 
         {
             $schedule->job(new SyncLilNounTotalSupply, 'lils')->everyTenMinutes();
             $schedule->job(new SyncLilNounTokenIdentities, 'lils')->everyTenMinutes();
