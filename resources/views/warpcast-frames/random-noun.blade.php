17b186edd7067d4bbda13164b846abf73a2332cf
Tom <tmaltby19@gmail.com>
2024-04-05 12:53:48 +0700

    frames testing

---
+++ resources/views/warpcast-frames/random-noun.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>probe.wtf</title>
+    <meta property="og:image" content="https://probewtf.fra1.cdn.digitaloceanspaces.com/staging/nouns/svgs/{{ $noun->token_id }}.svg" />
+
+    <!-- Required Frame Properties -->
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="https://probewtf.fra1.cdn.digitaloceanspaces.com/staging/nouns/svgs/{{ $noun->token_id }}.svg" />    
+
+    <!-- Optional Properties for Interaction -->
+    <meta property="fc:frame:button:1" content="Probe Nouns" />
+    <meta property="fc:frame:button:1:action" content="link" />
+    <meta property="fc:frame:button:1:post_url" content="https://www.probe.wtf/warpcast-frames/probe" />
+</head>
+<body>
+    <h1>probe.wtf</h1>
+</body>
+</html>
