da1139e776ccdda4d92bd35875811ec1b0c93199
Tom <tmaltby19@gmail.com>
2024-04-05 23:10:26 +0700

    add gif

--- resources/views/warpcast-frames/lil-nouns/gifs/get-funding-2day.blade.php
+++ resources/views/warpcast-frames/lil-nouns/gifs/get-funding-2day.blade.php
@@ -8,6 +8,7 @@
     <!-- Required Frame Properties -->
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://media1.giphy.com/media/nk5GmpufiGkJb2eE9P/giphy.gif?cid=6c09b952ewk3mawwmgcpkri8fcyni0odostsle7eu4gr58lp&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g" />    
+    <meta property="fc:frame:image:aspect_ratio" content="1:1" />
 
     <!-- Optional Properties for Interaction -->
     <meta property="fc:frame:button:1" content="lilnouns.wtf" />
