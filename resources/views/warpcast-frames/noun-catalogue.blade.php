5e3b3563ba11ade16f2a0140d40d97dfdbac4fbb
Tom <tmaltby19@gmail.com>
2024-04-05 21:09:20 +0700

    testing

--- resources/views/warpcast-frames/noun-catalogue.blade.php
+++ resources/views/warpcast-frames/noun-catalogue.blade.php
@@ -3,23 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <title>probe.wtf</title>
-    <meta name="og:image" content="{{ $nounPng }}" />
+    <meta property="og:image" content="{{ $nounPng }}" />
 
     <!-- Required Frame Properties -->
-    <meta name="fc:frame" content="vNext" />
-    <meta name="fc:frame:image" content="{{ $nounPng }}" />    
-    <meta name="fc:frame:image:aspect_ratio" content="1:1" />
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="{{ $nounPng }}" />    
+    <meta property="fc:frame:image:aspect_ratio" content="1:1" />
 
     <!-- Optional Properties for Interaction -->
-    <meta name="fc:frame:button:1" content="{{ $noun->token_id }}" />
-    <meta name="fc:frame:button:1:action" content="link" />
-    <meta name="fc:frame:button:1:target" content="https://nouns.wtf/noun/{{ $noun->token_id }}" />
+    <meta property="fc:frame:button:1" content="{{ $noun->token_id }}" />
+    <meta property="fc:frame:button:1:action" content="link" />
+    <meta property="fc:frame:button:1:target" content="https://nouns.wtf/noun/{{ $noun->token_id }}" />
 
-    <meta name="fc:frame:button:2" content="probe.wtf" />
-    <meta name="fc:frame:button:2:action" content="link" />
-    <meta name="fc:frame:button:2:target" content="https://probe.wtf/nouns" />
+    <meta property="fc:frame:button:2" content="probe.wtf" />
+    <meta property="fc:frame:button:2:action" content="link" />
+    <meta property="fc:frame:button:2:target" content="https://probe.wtf/nouns" />
 
-    @if ($noun->token_id >= 0)
+    @if ($hasMore)
+        <meta property="fc:frame:button:3" content="Next" />
+        <meta property="fc:frame:button:3:action" content="post" />
+        <meta property="fc:frame:button:3:post_url" content="https://api.probe.wtf/api/warpcast-frames/next-noun/{{ $noun->token_id }}" />
+    @endif
+
+    {{-- @if ($noun->token_id >= 0)
         @if ($noun->token_id > 0)
             <meta name="fc:frame:button:3" content="Previous" />
             <meta name="fc:frame:button:3:action" content="post" />
@@ -35,7 +41,7 @@
             <meta name="fc:frame:button:3:action" content="post" />
             <meta name="fc:frame:button:3:post_url" content="https://api.probe.wtf/api/warpcast-frames/next-noun/{{ $noun->token_id }}" />
         @endif
-    @endif
+    @endif --}}
 </head>
 <body>
     <p>go to <a href="https://www.probe.wtf">probe.wtf</a></p>
