9d8e4493ded37a36485ab250332a6e451370662d
Tom <tmaltby19@gmail.com>
2024-04-30 12:47:15 +0700

    refresh png path, svg path

--- app/Jobs/Noun/UpdateNounTokenID.php
+++ app/Jobs/Noun/UpdateNounTokenID.php
@@ -61,6 +61,8 @@ class UpdateNounTokenID implements ShouldQueue
                         'color_histogram' => null,
                         'area' => null,
                         'weight' => null,
+                        'svg_path' => null,
+                        'png_path' => null,
                     ]);
                 } else {
                     $noun->update([
